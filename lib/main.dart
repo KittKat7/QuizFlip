aaae1b426b7765deb70356b73c5f762b0e17f783
KittKat <122490347+KittKat7@users.noreply.github.com>
2026-02-09 09:57:25 -0500

    Added theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:kittkatflutterlibrary/kittkatflutterlibrary.dart';
 
 import 'models/cardlist.dart';
 import 'pages/homepage.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  AppTheme appTheme = AppTheme();
   CardList.getMaster();
-  runApp(const MyApp());
+  runApp(ThemedWidget(widget: MyApp(), theme: appTheme));
 }
 
 class MyApp extends StatelessWidget {
@@ -16,24 +19,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: appTheme.getThemeDataLight(context),
+      darkTheme: appTheme.getThemeDataDark(context),
+      themeMode: appTheme.getThemeMode(context),
       home: const HomePage(),
     );
   }
